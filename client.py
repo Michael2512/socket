ede0a45671ed4a828dfe4953b2e08febc5c234de
Michael2512 <gmichael025@gmail.com>
2021-02-10 20:53:49 +0100

    codice commentato

--- client.py
+++ client.py
@@ -13,7 +13,7 @@ print(output_string)
 import socket
 
 SERVER_ADDRESS = '127.0.0.1'
-SERVER_PORT = 22224
+SERVER_PORT = 22224 #comunicare sulla stessa porta
 
 sock_service = socket.socket()
 
@@ -22,20 +22,20 @@ sock_service.connect((SERVER_ADDRESS, SERVER_PORT))
 print("Connesso a " + str((SERVER_ADDRESS, SERVER_PORT)))
 while True:
     try:
-        dati = input("Inserisci i dati da inviare (0 per terminare la connessione): ")
+        dati = input("Inserisci i dati da inviare (0 per terminare la connessione): ")  #chiedere di inserire i dati
     except EOFError:
-        print("\nOkay. Exit")
+        print("\nOkay. Exit") #se ci sono errori uscire
         break
     if not dati:
-        print("Non puoi inviare una stringa vuota!")
+        print("Non puoi inviare una stringa vuota!")  #se non ci sono dati dire quella frase
         continue
     if dati == '0':
-        print("Chiudo la connessione con il server!")
+        print("Chiudo la connessione con il server!") #se i dati sono 0 chiudere la comunicazioni
         break
     
-    dati = dati.encode()
+    dati = dati.encode()  #dato che tra le socket le stringhe ecc vengono codificate in nyte server per decodificarli
 
-    sock_service.send(dati)
+    sock_service.send(dati) #mandare i dati
 
     dati = sock_service.recv(2048)
 
@@ -43,9 +43,9 @@ while True:
         print("Server non risponde. Exit")
         break
     
-    dati = dati.decode()
+    dati = dati.decode() #decodificare i dati
 
-    print("Ricevuto dal server:")
-    print(dati + '\n')
+    print("Ricevuto dal server:")  #output ricevuto dal server
+    print(dati + '\n') 
 
 sock_service.close()
